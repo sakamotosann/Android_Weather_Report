270b591297ffc4ede2336bdac04569e08fe99ca5
sakamotosann <15311937017@126.com>
2018-12-31 15:55:06 +0800

    2018-12-31 15:54:00

--- app/src/main/java/com/pby/weather_report/MainActivity.java
+++ app/src/main/java/com/pby/weather_report/MainActivity.java
@@ -1,13 +1,23 @@
 package com.pby.weather_report;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            System.out.println("运行1");
+            startActivity(intent);
+            finish();
+        }
     }
+
 }
